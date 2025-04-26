b9411c555fcc5f8b0b2acf5fd2da09145d10eaa0
Jorddi Suarez <jorsuap@gmail.com>
2025-04-26 14:19:38 -0500

    mf hot and mf

---
+++ projects/mf/src/app/show-info/show-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-info.component.html',
+  styleUrl: './show-info.component.scss'
+})
+export class ShowInfoComponent {
+
+}
