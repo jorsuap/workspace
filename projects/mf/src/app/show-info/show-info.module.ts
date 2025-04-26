6815665314d9116fac1a74fd85a1de1b2bb783e6
Jorddi Suarez <jorsuap@gmail.com>
2025-04-26 16:01:04 -0500

    mf restar

---
+++ projects/mf/src/app/show-info/show-info.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShowInfoRoutingModule } from './show-info-routing.module';
+import { ShowInfoComponent } from './show-info.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ShowInfoRoutingModule,
+    ShowInfoComponent
+  ]
+})
+export class ShowInfoModule { }
